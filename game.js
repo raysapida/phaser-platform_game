5436fa44c24d8cb76e01f672bd5492e8e320aec3
Raymond Sapida <ray.sapida@gmail.com>
2015-06-13 04:02:41 -0700

    Add tilemap to preloader and game js

--- game.js
+++ game.js
@@ -3,16 +3,19 @@
 };
 
 GameStates.Game.prototype = {
+  var map;
+  var backgroundLayer;
+  var blockLayer;
 
-    create: function () {
-        //below code creates a simple tween animation. You will want to delete this when adding your code
-        var logo = this.add.sprite(this.world.centerX, this.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
-        logo.scale.setTo(0.2, 0.2);
-        this.add.tween(logo.scale).to({ x: 1, y: 1 }, 2000, Phaser.Easing.Bounce.Out, true);
-    },
+  create: function () {
+    map = this.add.tilemap('myTilemap');
+    map.addTilesetImage('scifi_platformTiles_32x32', 'myTileset');
 
-    update: function () { },
+    backgroundLayer = map.createLayer('background');
+    blockLayer = map.createLayerteLayer('blocklayer');
+  },
 
-    render: function () { },
+  update: function () { },
+
+  render: function () { },
 };
--- preloader.js
+++ preloader.js
@@ -1,21 +1,23 @@
 ﻿// Preloader will load all of the assets like graphics and audio
 GameStates.Preloader = function (game) {
-    this.preloadBar = null;
+  this.preloadBar = null;
 }
 
 GameStates.Preloader.prototype = {
-    preload: function () {
-        // common to add a loading bar sprite here...
-        this.preloadBar = this.add.sprite(this.game.width / 2 - 100, this.game.height / 2, 'preloaderBar');
-        this.load.setPreloadSprite(this.preloadBar);
-        // load all game assets
-        // images, spritesheets, atlases, audio etc..
-        this.load.image('logo', 'assets/phaser2.png');
-    },
+  preload: function () {
+    // common to add a loading bar sprite here...
+    this.preloadBar = this.add.sprite(this.game.width / 2 - 100, this.game.height / 2, 'preloaderBar');
+    this.load.setPreloadSprite(this.preloadBar);
+    // load all game assets
+    // images, spritesheets, atlases, audio etc..
+    this.load.image('logo', 'assets/phaser2.png');
+    this.load.tilemap('myTilemap', 'assets/tilemaps/scifi.json', null, Phaser.Tilemap.TILED_JSON);
+    this.load.image('myTileset', "assets/tilemaps/scifi_platformTiles_32x32.png");
+  },
 
     create: function () {
-        //call next state
-        //this.state.start('MainMenu');
-        this.state.start('Game');
+      //call next state
+      //this.state.start('MainMenu');
+      this.state.start('Game');
     }
 };
